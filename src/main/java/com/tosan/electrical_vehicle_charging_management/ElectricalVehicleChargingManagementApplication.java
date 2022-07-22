1af6c1d613a9d573ac63182e91f101d84abfbfcb
Atieh <atieh.rahbari@gmail.com>
2022-07-22 13:08:05 +0330

    start project electrical_vehicle_charging_management

---
+++ src/main/java/com/tosan/electrical_vehicle_charging_management/ElectricalVehicleChargingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tosan.electrical_vehicle_charging_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricalVehicleChargingManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectricalVehicleChargingManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tosan/electrical_vehicle_charging_management/ElectricalVehicleChargingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tosan.electrical_vehicle_charging_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricalVehicleChargingManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
