a5e85e3bb76b89a4179fcf006a0be06e54742d1c
Atieh <atieh.rahbari@gmail.com>
2022-07-22 13:59:17 +0330

    modify save in CompanyServiceImpl

--- src/main/java/com/tosan/electrical_vehicle_charging_management/service/CompanyServiceImpl.java
+++ src/main/java/com/tosan/electrical_vehicle_charging_management/service/CompanyServiceImpl.java
@@ -3,16 +3,33 @@ package com.tosan.electrical_vehicle_charging_management.service;
 import com.tosan.electrical_vehicle_charging_management.data.entity.ParentCompany;
 import com.tosan.electrical_vehicle_charging_management.data.entity.Station;
 import com.tosan.electrical_vehicle_charging_management.dto.ParentCompanyDto;
+import com.tosan.electrical_vehicle_charging_management.dto.mapper.CompanyMapper;
+import com.tosan.electrical_vehicle_charging_management.repository.CompanyRepository;
+import com.tosan.electrical_vehicle_charging_management.service.exception.EntityIsExistException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.Set;
 
 /**
  * @author arm
  */
+@Service
+@AllArgsConstructor
 public class CompanyServiceImpl implements CompanyService{
+    private final CompanyMapper companyMapper;
+    private final CompanyRepository companyRepository;
     @Override
     public boolean save(ParentCompanyDto parentCompanyDto) {
-        return false;
+        ParentCompany parentCompany1 = companyMapper.toParentCompany(parentCompanyDto);
+        Optional<ParentCompany> foundedCompany = companyRepository.findByName(parentCompany1.getName());
+        if (foundedCompany.isPresent()) {
+            throw new EntityIsExistException("this company name exist!");
+        } else {
+            companyRepository.save(parentCompany1);
+            return true;
+        }
     }
 
     @Override
