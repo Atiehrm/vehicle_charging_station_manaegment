e89a54a364f06f5c1e10c7867216fa085253cd86
Atieh <atieh.rahbari@gmail.com>
2022-07-22 14:42:16 +0330

    add AddCompanyTest

---
+++ src/test/java/com/tosan/electrical_vehicle_charging_management/service/company/AddCompanyTest.java
@@ -0,0 +1,40 @@
+package com.tosan.electrical_vehicle_charging_management.service.company;
+
+import com.tosan.electrical_vehicle_charging_management.dto.ParentCompanyDto;
+import com.tosan.electrical_vehicle_charging_management.service.CompanyService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.servlet.jsp.jstl.core.Config;
+
+/**
+ * @author arm
+ */
+public class AddCompanyTest {
+    ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+    CompanyService companyService = context.getBean(CompanyService.class);
+    ParentCompanyDto parentCompany;
+    ParentCompanyDto parentCompany1;
+
+    @BeforeEach
+    public void init() {
+        parentCompany = ParentCompanyDto.builder().build();
+        parentCompany1 = ParentCompanyDto.builder().build();
+
+    }
+
+    @Test
+    void givenNotDuplicateCompany_WhenAddNewCompanyCalls_ThenReturnTrueResponse() {
+        boolean result = companyService.save(parentCompany);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void givenDuplicateCompany_WhenAddNewCompanyCalls_ThenReturnTrueResponse() {
+        boolean result = companyService.save(parentCompany1);
+        Assertions.assertFalse(result);
+    }
+}
