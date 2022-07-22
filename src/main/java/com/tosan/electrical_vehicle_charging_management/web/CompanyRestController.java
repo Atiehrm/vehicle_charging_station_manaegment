8af68b18ab1e7e638bade8989c197e80b17fcca0
Atieh <atieh.rahbari@gmail.com>
2022-07-22 14:24:02 +0330

    add getAllCompaniesInfo in CompanyRestcontroller

--- src/main/java/com/tosan/electrical_vehicle_charging_management/web/CompanyRestController.java
+++ src/main/java/com/tosan/electrical_vehicle_charging_management/web/CompanyRestController.java
@@ -1,7 +1,44 @@
 package com.tosan.electrical_vehicle_charging_management.web;
 
+import com.tosan.electrical_vehicle_charging_management.dto.ParentCompanyDto;
+import com.tosan.electrical_vehicle_charging_management.service.CompanyService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
 /**
  * @author arm
  */
+@RestController
+@RequestMapping("/company")
+@RequiredArgsConstructor
+@Api(value = "CompanyRestController")
 public class CompanyRestController {
+    private final CompanyService companyService;
+
+
+    @ApiOperation(value = "Get list of Company info ", response = Iterable.class, tags = "getCompanyInfos")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 400, message = "validation error!"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!")})
+    @GetMapping(value = "/AllCompaniesInfo"
+            , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Set<ParentCompanyDto>> getAllCompaniesInfo
+            (@RequestBody ParentCompanyDto dto) {
+        return new ResponseEntity(companyService.getCompaniesInformation(), HttpStatus.OK);
+    }
 }
