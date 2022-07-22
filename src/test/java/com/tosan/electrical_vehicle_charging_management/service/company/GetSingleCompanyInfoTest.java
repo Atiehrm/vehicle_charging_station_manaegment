ce5febb2b40aad4cfb2475de6df062f12e519871
Atieh <atieh.rahbari@gmail.com>
2022-07-22 14:45:40 +0330

    add GetSingleCompanyInfoTest

---
+++ src/test/java/com/tosan/electrical_vehicle_charging_management/service/company/GetSingleCompanyInfoTest.java
@@ -0,0 +1,44 @@
+package com.tosan.electrical_vehicle_charging_management.service.company;
+
+import com.tosan.electrical_vehicle_charging_management.data.entity.ParentCompany;
+import com.tosan.electrical_vehicle_charging_management.dto.ParentCompanyDto;
+import com.tosan.electrical_vehicle_charging_management.service.CompanyService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.servlet.jsp.jstl.core.Config;
+import java.util.Optional;
+
+/**
+ * @author arm
+ */
+public class GetSingleCompanyInfoTest {
+    ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+    CompanyService companyService = context.getBean(CompanyService.class);
+    ParentCompanyDto parentCompany;
+    ParentCompanyDto parentCompany1;
+
+    @BeforeEach
+    public void init() {
+        parentCompany = ParentCompanyDto.builder().build();
+        parentCompany1 = ParentCompanyDto.builder().build();
+
+    }
+
+    @Test
+    void givenNotExistCompany_WhenGetSingleCompanyInfoCalls_ThenReturnFalseResponse() {
+        Optional<ParentCompany> result = Optional.ofNullable(companyService.findSingleCompanyByName(parentCompany.getName()));
+        if (result.isEmpty())
+            Assertions.fail();
+    }
+
+    @Test
+    void givenExistCompany_WhenGetSingleCompanyInfoCalls_ThenReturnTrueResponse() {
+        Optional<ParentCompany> result = Optional.ofNullable(companyService.findSingleCompanyByName(parentCompany1.getName()));
+        if (result.isPresent())
+            Assertions.assertTrue(true);
+    }
+}
